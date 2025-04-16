a381ea9fc7a677e458e09455742a67fd497c659e
rohan <rohanrmanu99@gmail.com>
2025-04-17 00:40:44 +0530

    UC-1 Added method to calculate Length

--- src/com/brlab/linecomparison/LineComparison.java
+++ src/com/brlab/linecomparison/LineComparison.java
@@ -1,7 +1,35 @@
 package com.brlab.linecomparison;
 
+class Point {
+    int x, y;
+    // constructor
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Line {
+    Point p1, p2;
+
+    public Line(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    // Line = sqrt( (x2 - x1) ^ 2 + (y2- y1) ^ 2)
+    public double calculateLength() {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+}
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        Point point1 = new Point(2,5);
+        Point point2 = new Point(-2,5);
+        Line line1 = new Line(point1,point2);
+        Line line2 = new Line(point1,point2);
+        System.out.println(line1.calculateLength());
     }
 }
